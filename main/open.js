5484ae8065e31684a67b0cd0c9be9b85f80bde20
nplawton <nplawton02@gmail.com>
2023-02-09 05:00:38 -0800

    Main page start

---
+++ main/open.js
@@ -0,0 +1,9 @@
+//functionality Test
+function test(){
+    console.log('This has been a test of the emergency broadcast service.');
+}
+
+let survey = $('#survey');
+survey.click(() => {
+    test();
+});
