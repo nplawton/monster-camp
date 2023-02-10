4ff5414016db4c02ea9a13d87deec20ff7a5fcbd
nplawton <nplawton02@gmail.com>
2023-02-10 05:33:34 -0800

    fixed button placement

--- main/open.js
+++ main/open.js
@@ -2,3 +2,24 @@
 function test(){
     console.log('This has been a test of the emergency broadcast service.');
 }
+
+let sound = {
+    door: new Howl({
+        src: ['../Sounds/door.mp3'],
+        autoplay: false,
+        loop: false
+
+    })
+}
+
+let adoor = $('.img-arena')
+
+adoor.click(() => {
+    sound.door.play()
+});
+
+let sdoor = $('.img-survey');
+
+sdoor.click(() => {
+    sound.door.play()
+});
